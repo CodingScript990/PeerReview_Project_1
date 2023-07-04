58f5050d863f51ff773e096b5f5b84f4833377c1
CodeScript990 <trte135@naver.com>
2023-07-04 23:15:06 +0900

    fix.7.4.2023 MiniProject - [Project Repository Update]  - Content :    1. Repository      - NegotiationRepository : Service에서 Read부분에 필요한 메서드를 추가함

--- market/src/main/java/com/likelion/market/repository/NegotiationRepository.java
+++ market/src/main/java/com/likelion/market/repository/NegotiationRepository.java
@@ -1,7 +1,15 @@
 package com.likelion.market.repository;
 
 import com.likelion.market.entity.NegotiationEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface NegotiationRepository extends JpaRepository<NegotiationEntity, Long> {
+    List<NegotiationEntity> findByItemId(Long itemId);
+    List<NegotiationEntity> findAllByItemId(Long itemId);
+    Page<NegotiationEntity> findAllByItemIdAndWriterAndPassword(Long itemId, String writer, String password, Pageable pageable);
+    Page<NegotiationEntity> findAllByItemId(Long itemId, Pageable pageable);
 }
